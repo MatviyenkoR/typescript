a73abd7d97e9bd1a6bed308b5dadf9ae165dd89b
SavchenkoT <tsavchenko@ngoar.net>
2017-05-19 15:40:19 +0300

    function-greeter

--- app/scripts/typescript/tetiana.ts
+++ app/scripts/typescript/tetiana.ts
@@ -1,9 +1,28 @@
 /**
 	* TypeScript file
   */
-  function greeter(person: string) {
-      return 'Hello,'  + person;
-  }
-  let user = 'Jane User';
+// function greeter(person : string) {
+//     return 'Hello,'  + person;
+// }
+// let users = 'Jane User';
 
-  document.body.innerText = greeter(user);
+// document.body.innerText = greeter(users);
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+class Student implements Person {
+  public fullName: string;
+  constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+      this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+      // document.body.innerText = this.fullName;
+  }
+  public greetersy(): string {
+    return 'Hello, ' + this.fullName;
+  }
+}
+let user = new Student('Jane', 'M.', 'User');
+
+document.body.innerText = user.greetersy();
