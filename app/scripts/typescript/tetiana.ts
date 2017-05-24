82ebb39a99e531abf2478ac0c0e1470ffd18a066
SavchenkoT <tsavchenko@ngoar.net>
2017-05-24 19:32:17 +0300

    commit

--- app/scripts/typescript/tetiana.ts
+++ app/scripts/typescript/tetiana.ts
@@ -1,28 +1,50 @@
 /**
 	* TypeScript file
   */
-module Validation {
-    export interface StringValidator {
-        isAcceptable(s: string): boolean;
+
+class Menu {
+  public items: string[];
+  public pages: number;
+
+  constructor(itemList: string[], totalPages: number) {
+    this.items = itemList;
+    this.pages = totalPages;
+  }
+  public list(): void {
+    console.log('Our menu for today: ');
+    for ( let i = 0; i < this.items.length; i = i + 1 ) {
+      console.log(this.items[i]);
     }
 }
 
-function printLabel(labelledObj: {label: string}) {
-  console.log(labelledObj.label);
-}
-let myObj = {size: 20, label: 'Size 20 Object'};
-printLabel(myObj);
+let sundayMenu = new Menu(['pancakes', 'waffles', 'orange juice'], 1);
+  sundayMenu.list();
+// function buildName(firstName: string, ...restOfName: string[]) {
+//  return firstName + ' ' + restOfName.join(' ');
+// }
+//
+// let employeeName = buildName('Joseph', 'Samuel', 'Lucas', 'MacKinzie');
+//
+// console.log(employeeName);
 
-interface LabelledValue {
-  label: string;
-}
+// function buildName (firstName: string, lastName?: string) {
+//     if (lastName) {return firstName + ' ' + lastName;
+//       } else { return firstName; }
+// }
+//
+// let result1 = buildName();  //работает
 
-function printLabel(labelledObj: LabelledValue) {
-  console.log(labelledObj.label);
-}
-
-let myObj = {size: 10, label: 'Size 10 Object'};
-printLabel(myObj);
+// interface ClockInterface {
+//     h (hour: number, minute: number);
+// }
+//
+// class Clock implements ClockInterface  {
+//     public currentTime: Date;
+//     constructor(hour: number, minute: number) {
+//       this.hour();
+//       this.minute();
+//      }
+// }
 
 // module User {
 //   class SomeType {
