05b6706df9ed80c3ed524f6077d80cf954501d29
SavchenkoT <tsavchenko@ngoar.net>
2017-06-05 12:29:39 +0300

    add-block.ts

---
+++ app/scripts/typescript/block.ts
@@ -0,0 +1,9 @@
+/**
+* TypeScript file
+*/
+
+let modalMenu = getClassName('.modal-menu');
+
+interface Color () {
+  
+}
--- app/scripts/typescript/tetiana.ts
+++ app/scripts/typescript/tetiana.ts
@@ -1,24 +1,55 @@
 /**
 	* TypeScript file
-  */
+*/
+class Student {
+    private firstName: string;
+    private lastName: string;
+    public yearOfBirth: number;
+    public schoolName: string;
+    public city: string;
 
-class Menu {
-  public items: string[];
-  public pages: number;
+    constructor( firstName: string = '...', lastName: string = '...', schoolName: string = '...', city: string = '...', yearOfBirth: number = 1990 ) {
 
-  constructor(itemList: string[], totalPages: number) {
-    this.items = itemList;
-    this.pages = totalPages;
-  }
-  public list(): void {
-    console.log('Our menu for today: ');
-    for ( let i = 0; i < this.items.length; i = i + 1 ) {
-      console.log(this.items[i]);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+        this.city = city;
+        this.schoolName = schoolName;
     }
+
+    // public age() {
+    //     return 2014 - this.yearOfBirth;
+    // }
+
+    public printStudentFullName(): void {
+        console.log( this.lastName + ',' + this.firstName, + this.yearOfBirth);
+    }
+
 }
 
-let sundayMenu = new Menu(['pancakes', 'waffles', 'orange juice'], 1);
-  sundayMenu.list();
+let student = new Student('Tom', 'Hanks', 'World Acting School');
+// let age = student.age();
+let fullName = student.printStudentFullName();
+let schoolName = student.schoolName;
+
+// class Menu {
+//   public items: string[];
+//   public pages: number;
+//
+//   constructor(itemList: string[], totalPages: number) {
+//     this.items = itemList;
+//     this.pages = totalPages;
+//   }
+//   public list(): void {
+//     console.log('Our menu for today: ');
+//     for ( let i = 0; i < this.items.length; i = i + 1 ) {
+//       console.log(this.items[i]);
+//     }
+// }
+//
+// let sundayMenu = new Menu(['pancakes', 'waffles', 'orange juice'], 1);
+//   sundayMenu.list();
+
 // function buildName(firstName: string, ...restOfName: string[]) {
 //  return firstName + ' ' + restOfName.join(' ');
 // }
@@ -32,7 +63,7 @@ let sundayMenu = new Menu(['pancakes', 'waffles', 'orange juice'], 1);
 //       } else { return firstName; }
 // }
 //
-// let result1 = buildName();  //работает
+// let result1 = buildName();
 
 // interface ClockInterface {
 //     h (hour: number, minute: number);
